93f0ae3bf74e7edf5127987fca81783e0e3a5dc5
Raihan Rafif <raihanrafif200095@gmail.com>
2023-11-03 17:20:45 +0700

    doing revision

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -20,14 +20,15 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation 'junit:junit:4.13'
+        testImplementation("junit:junit:4.13")
+
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api("org.apache.commons:commons-math3:3.6.1")
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation("com.google.guava:guava:32.1.1-jre")
-    implementation 'com.google.guava:guava:29.0-jre'
+    implementation("com.google.guava:guava:29.0-jre")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -39,7 +40,7 @@ java {
 
 tasks.register("greetingTask") {
     doLast {
-        val nama = project.findProperty("nama") ?: "Gradle User"
+        val nama = project.findProperty("nama") as String? ?: "Gradle User"
         println("Hello, $nama! Welcome to Gradle World!")
     }
 }
---
+++ readme.md
@@ -0,0 +1,12 @@
+Kriteria:
+
+- buat tugas Gradle sederhana untuk menerima parameter CLI dan mencetaknya dengan pesan ucapan
+- Proyek skrip Gradle harus dibuat di repositori yang berbeda dan dorong ke GitHub
+
+How to run the custom gradle :
+    - go to the ./tutorial-gradle directory in command
+    - insert command "./gradlew greetingTask -Pnama=YourName"
+    - click enter
+
+note:
+Screenshots of the running results are in the results folder
